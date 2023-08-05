4b871efd5c5d1dbbbc4acced350ceb352dea65e9
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-08-05 16:50:42 +0200

    display skills list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { skillsServices } from "./services/skills.services";
+
+const Skill = ({ skills, title }) => {
+  return (
+    <div>
+      <h3>{title} Skills</h3>
+      {skills.map((skill) => (
+        <p>{skill.name}</p>
+      ))}
+    </div>
+  );
+};
 
 function App() {
-  return <div>hello</div>;
+  const [skills, setSkills] = useState();
+
+  const getAllSkills = async () => {
+    const data = await skillsServices.getAllSkills();
+    // console.log(data);
+    setSkills(data);
+    return data;
+  };
+
+  useEffect(() => {
+    getAllSkills();
+  }, []);
+
+  return (
+    <div>
+      {skills && (
+        <div>
+          <Skill title="Principal" skills={skills.compulsory}></Skill>
+          <Skill title="Basic" skills={skills.basic}></Skill>
+          <Skill title="Personal" skills={skills.personal}></Skill>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/services/skills.services.js
@@ -0,0 +1,11 @@
+import { apiUrl } from "./constants";
+
+const getAllSkills = async () => {
+  const url = `${apiUrl}all-skills`;
+  const response = await fetch(url);
+  return response.json();
+};
+
+export const skillsServices = {
+    getAllSkills,
+};
